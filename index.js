a8186dc1da38289091b2f51562eb050bc06e3329
Dillon Titcomb <dillontitcomb@gmail.com>
2019-03-26 20:01:50 -0700

    Add example popups for Saint Louis and Minneapolis, and add location information to information div on popup click.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const newDiv = document.getElementById('hello');
-newDiv.innerHTML = 'SCRIPT WORKS';
-
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+let infoDiv = document.getElementById('mapinfo');
+const mymap = L.map('mapid').setView([39.627, -90.1994], 6);
 
 L.tileLayer(
   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
@@ -9,10 +7,32 @@ L.tileLayer(
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.satellite',
+    id: 'mapbox.streets',
     accessToken:
       'pk.eyJ1IjoiZGlsbG9udDM0IiwiYSI6ImNqdHFrY3h4ODBnYjU0NG5zb2R3Y3dkbTEifQ.3lQum1l79lsJhjygxIoRrg'
   }
 ).addTo(mymap);
 
+const minneapolis = L.marker([44.9, -93.2]).addTo(mymap);
+const mplsFlow = 1245;
+const mplsHTML = `<p>Site: <strong>Minneapolis</strong></p><p>Current flow: <strong>${mplsFlow} ft3/s</strong></p>`;
+minneapolis.bindPopup(mplsHTML);
+
+const stLouis = L.marker([38.6, -90.2]).addTo(mymap);
+const stLouisFlow = 3250;
+const stLouisHTML = `<p>Site: <strong>St. Louis</strong></p><p>Current flow: <strong>${stLouisFlow} ft3/s</strong></p>`;
+stLouis.bindPopup(stLouisHTML);
+
+function onMplsClick(e) {
+  infoDiv.innerHTML = mplsHTML;
+  console.log(e);
+}
+
+function onStLouisClick(e) {
+  infoDiv.innerHTML = stLouisHTML;
+  console.log(e);
+}
+
+minneapolis.on('click', onMplsClick);
+stLouis.on('click', onStLouisClick);
 console.log(mymap);
